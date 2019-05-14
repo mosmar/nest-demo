fd45c01f99f3fdf3eb0e1025b236b2ece63d15cf
Micca Osmar <mosmar@pop-os.localdomain>
2019-05-13 22:04:02 -0400

    Updated passenger module with referene to passenger entity

--- src/passenger/passenger.module.ts
+++ src/passenger/passenger.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassengerController } from './passenger.controller';
 import { PassengerService } from './passenger.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassengerEntity } from './passenger.entity';
 
 @Module({
-  controllers: [PassengerController],
-  providers: [PassengerService]
+ imports: [TypeOrmModule.forFeature([PassengerEntity])],
+ controllers: [PassengerController],
+ providers: [PassengerService]
 })
 export class PassengerModule {}
