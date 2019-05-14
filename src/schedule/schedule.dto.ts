bd7da33e680a5028cb85f9e6703d66bf862404b9
Micca Osmar <mosmar@pop-os.localdomain>
2019-05-13 21:09:22 -0400

    Generated schedule entity and dto

---
+++ src/schedule/schedule.dto.ts
@@ -0,0 +1,5 @@
+export interface ScheduleDTO {
+    arrivalTime: Date;
+    departureTime: Date;
+ }
+ 
---
+++ src/schedule/schedule.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class ScheduleEntity {
+   @PrimaryGeneratedColumn('uuid')
+   id: string;
+
+   @Column({type: 'date'})
+   departureTime: Date;
+
+   @Column({type: 'date'})
+   arrivalTime: Date;
+}
